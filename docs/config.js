41f21e7215881ae1818126fe87c848127d1097c3
hhyunn2 <hyunin9528@gmail.com>
2026-05-03 22:40:26 +0900

    Connect survey to Google Apps Script

--- docs/config.js
+++ docs/config.js
@@ -1,6 +1,6 @@
 window.SURVEY_CONFIG = {
   githubUser: "hhyunn2",
-  submitEndpoint: "",
+  submitEndpoint: "https://script.google.com/macros/s/AKfycby8vEeBUT-ai7-NeEovGZIlXHUwp7IMeFuTzxS-HEc4KAZNCVumxNxpPAvTpiv2aUI8/exec",
   spreadsheetUrl: "https://docs.google.com/spreadsheets/d/1U1kb6I9J7iWEurvBBCKeUGpkgCdIPwaK6RuRRHnaW0o/edit?usp=sharing",
   noCors: true,
   defaultImage: "assets/default-image.svg",
