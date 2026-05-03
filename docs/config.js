6f85515e92df76880c830faf80b34e3f1505748f
hhyunn2 <hyunin9528@gmail.com>
2026-05-03 20:17:08 +0900

    Create survey GitHub Pages site

--- docs/config.js
+++ docs/config.js
@@ -1,6 +1,7 @@
 window.SURVEY_CONFIG = {
   githubUser: "hhyunn2",
   submitEndpoint: "",
+  spreadsheetUrl: "https://docs.google.com/spreadsheets/d/1U1kb6I9J7iWEurvBBCKeUGpkgCdIPwaK6RuRRHnaW0o/edit?usp=sharing",
   noCors: true,
   defaultImage: "assets/default-image.svg",
   storageKey: "expert-style-survey-draft"
